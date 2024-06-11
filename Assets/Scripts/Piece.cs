691a67aaeba13f60dfacf0726e8a4dd090dddb5d
tomoyu719 <tomoyuki719245@gmail.com>
2024-06-11 10:59:04 +0900

    Revert "change hard drop key"
    This reverts commit 71fd37b4a052bc247aec40554d9e34e520db7d14.

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -52,7 +52,7 @@ public class Piece : MonoBehaviour
         }
 
         // Handle hard drop
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             HardDrop();
         }
 
